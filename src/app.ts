52a918f0e8b997383faf9b92262c83742794be85
Shailesh Aanand <anaandshailu@gmail.com>
2024-02-13 14:10:03 +0530

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,31 @@
 import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import doctorsController from "./controllers/doctor";
+import { ZodError } from "zod";
+import { APIError } from "./errors";
 
 const app = new Elysia()
   .use(swagger())
   .get("/", () => {
     return { hello: "world" };
+  })
+  .onError(({ error, code, set }) => {
+    if (error instanceof ZodError) {
+      set.status = 400;
+      return {
+        errors: error.errors.map((e) => ({ message: e.message })),
+      };
+    }
+    if (code === "VALIDATION") {
+      set.status = 400;
+      return { errors: [{ message: "Validation Error" }] };
+    }
+    if (error instanceof APIError) {
+      set.status = error.status;
+      return { errors: [{ message: error.message }] };
+    }
+    // biome-ignore lint/suspicious/noConsoleLog: needs logging
+    console.log(error);
   })
   .use(doctorsController);
 
