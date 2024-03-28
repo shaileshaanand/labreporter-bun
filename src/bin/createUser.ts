182c9b7daefec4000fa0797aadec26dfaf634f73
Shailesh Aanand <anaandshailu@gmail.com>
2024-03-28 10:12:40 +0530

    add createUser script

---
+++ src/bin/createUser.ts
@@ -0,0 +1,49 @@
+import db from "../context/db";
+
+import { parseArgs } from "util";
+import { users } from "../db/schema";
+import { hashPassword } from "../helpers";
+
+const {
+  values: { firstName, lastName, username, password },
+} = parseArgs({
+  options: {
+    firstName: {
+      type: "string",
+      short: "f",
+    },
+    lastName: {
+      type: "string",
+      short: "l",
+    },
+    username: {
+      type: "string",
+      short: "u",
+    },
+    password: {
+      type: "string",
+      short: "p",
+    },
+  },
+});
+
+if (!firstName || !lastName || !username || !password) {
+  // biome-ignore lint/suspicious/noConsoleLog: needs logging
+  console.log(
+    "Usage: createUser -f <firstName> -l <lastName> -u <username> -p <password>",
+  );
+  process.exit(1);
+}
+
+// biome-ignore lint/suspicious/noConsoleLog: needs logging
+console.log(
+  await db
+    .insert(users)
+    .values({
+      firstName,
+      lastName,
+      username,
+      passwordHash: await hashPassword(password),
+    })
+    .returning(),
+);
