cb0289a6e7bf495d232b4565811db072374fbb04
Shailesh Aanand <anaandshailu@gmail.com>
2024-03-29 09:41:36 +0530

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import app from "./app";
-const PORT = process.env.port ?? 3000;
+const PORT = process.env.PORT ?? 3000;
 // biome-ignore lint/suspicious/noConsoleLog: needs logging
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
